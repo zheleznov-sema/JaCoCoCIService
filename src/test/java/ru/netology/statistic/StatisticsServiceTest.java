9bbf7f2e6825007cec77c778f782149c2ae34cec
zheleznov-sema <zheleznov.s@bk.ru>
2025-11-06 09:12:06 +0300

    added coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,9 +6,18 @@ import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
     @Test
-    void findMax() {
+   public void findMax() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxIfcurrentMax(){
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
         long actual = service.findMax(incomesInBillions);
         Assertions.assertEquals(expected, actual);
