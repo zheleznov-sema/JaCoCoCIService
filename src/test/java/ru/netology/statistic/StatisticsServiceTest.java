a2df04199a09c6133d83d279f07a9fc52a31472d
zheleznov-sema <zheleznov.s@bk.ru>
2025-11-09 12:50:54 +0300

    Added build crash when coverage is less than 100%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
     @Test
-   public void findMax() {
+    public void findMax() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
@@ -15,7 +15,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    public void shouldFindMaxIfcurrentMax(){
+    public void shouldFindMaxIfcurrentMax() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
